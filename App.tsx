952b7688d7d6235a00be42cf910da0c5c56e971c
dev.weisseneger <140491527+weissenegerdev@users.noreply.github.com>
2025-01-14 19:23:09 +0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HomeScreen } from './src/screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{
+            title: '1 Milyon TL Harcama',
+            headerStyle: {
+              backgroundColor: '#fff',
+            },
+            headerTintColor: '#2c3e50',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
